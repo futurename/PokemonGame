f4c79b8bc0f2bef35efbaa3c70cc04372a614cfa
Samaneh Shirdel <sam_pm17@yahoo.com>
2021-03-11 15:37:39 -0500

    update player

--- Player.h
+++ Player.h
@@ -10,19 +10,17 @@ class Player {
 private:
     string name;
     vector<Pokemon> playerPokemons;
-
+    int playerScore;
 
 public:
     Player(string name);
-
     string getName() const;
-
     vector<Pokemon> getPokemons();
-
+    int getPlayerScore();
+    void setPlayerScore(int score);
     void addPokenmon(Pokemon pokemon);
-
     bool removePokemon(Pokemon pokemon);
-
     void recoverBloodPoints(double ratio);
+
 };
 
