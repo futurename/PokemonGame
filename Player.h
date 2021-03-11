cef812b032cf2327d248243d7d6d7486eafe9b0d
Wei <=>
2021-03-11 13:18:39 -0500

    player modify

--- Player.h
+++ Player.h
@@ -1,15 +1,28 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include "Pokemon.h"
 
 using namespace std;
 
 class Player {
 private:
     string name;
+    vector<Pokemon> playerPokemons;
+
 
 public:
     Player(string name);
+
     string getName() const;
+
+    vector<Pokemon> getPokemons();
+
+    void addPokenmon(Pokemon pokemon);
+
+    bool removePokemon(Pokemon pokemon);
+
+    void recoverBloodPoints(double ratio);
 };
 
