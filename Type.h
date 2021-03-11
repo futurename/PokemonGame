974c627929c6039cde023c290baba419d1ddcad9
Wei <=>
2021-03-11 13:13:43 -0500

    running version

--- Type.h
+++ Type.h
@@ -1,18 +1,21 @@
 #pragma  once
 
-enum Type{GRASS, POISON, ROCK}; // not complete
+#include <iostream>
 
-static Type getTypeFromNumber(int typeNum){
+using namespace std;
+
+enum Type {
+    GRASS, POISON, ROCK
+}; // not complete
+
+static Type getTypeFromNumber(int typeNum) {
     switch (typeNum) {
         case 0:
             return GRASS;
-            break;
         case 1:
             return POISON;
-            break;
         case 2:
             return ROCK;
-            break;
         default:
             cout << "No such type number: " + to_string(typeNum) << endl;
             break;
